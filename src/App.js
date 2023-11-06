a5a77f778bdf7626c6a04c5f0997507025a8412a
Ranjith Gopinath <kingranjith4@gmail.com>
2023-11-05 20:33:48 -0800

    11-5-2

--- src/App.js
+++ src/App.js
@@ -5,17 +5,71 @@ import msgIcon from './assets/message.svg';
 import home from './assets/home.svg';
 import saved from './assets/bookmark.svg';
 import rocket from './assets/rocket.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
+import { sendMsgToOpenAI } from './openai';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+    const msgEnd = useRef(null);
+       
+     const [input, setInput] = useState("");
+     const [messages, setMessages ] = useState([
+         {
+          text: "This is just clone of the chatGPT using react html css and OpenAPI",
+          isBot: true,
+         }
+    ]);
+
+
+    useEffect(()=>{
+      msgEnd.current.scrollIntoView();
+    }, [messages])
+
+     const handleSend = async () => {
+        const text = input;
+        setInput('');
+        setMessages([
+          ...messages,
+          { text, isBot:false }
+        ])
+         const res = await sendMsgToOpenAI(text);
+         setMessages([
+          ...messages,
+          { text, isBot: false},
+          {text : res, isBot: true}
+        ]);
+     }
+
+     const handleEnter = async(e) => {
+      if(e.key === 'Enter') await handleSend();
+     }
+
+     const handleQuery = async (e) => {
+       const text = e.target.value;
+       setInput('');
+       setMessages([
+         ...messages,
+         { text, isBot:false }
+       ])
+        const res = await sendMsgToOpenAI(text);
+        setMessages([
+         ...messages,
+         { text, isBot: false},
+         {text : res, isBot: true}
+       ]);
+     }
+
   return (
     <div className="App">
       <div className="sideBar">
         <div className="upperSide">
             <div className="upperSideTop"><img src={gptLogo} alt="Logo" className="logo" /><span className="brand">ChatGPT</span></div>
-            <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
+            <button className="midBtn" onClick={()=>{window.location.reload()}}><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
             <div className="upperSideBottom">
-              <button className="query"><img src={msgIcon} alt="Query" />What is Programming ?</button>
-              <button className="query"><img src={msgIcon} alt="Query" />How to use an API ?</button>
+              <button className="query" onClick={handleQuery} value={"What is Programming?"}><img src={msgIcon} alt="Query" />What is Programming ?</button>
+              <button className="query" onClick={handleQuery} value={"How to use an API ?"}><img src={msgIcon} alt="Query" />How to use an API ?</button>
             </div>
         </div>
         <div className="lowerSide">
@@ -25,7 +79,20 @@ function App() {
         </div>
       </div>
       <div className="main">
-
+        <div className="chats">
+         {messages.map((message, i) => 
+               <div key={i} className={ message.isBot?"chat bot":"chat"}>
+                  <img className='chatImg' src={message.isBot?gptImgLogo:userIcon} alt="" /><p className="txt">{ message.text }</p>
+              </div>
+         )}
+         <div ref={msgEnd}/>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder='Send a message' value={input} onKeyDown={handleEnter} onChange={(e)=>{setInput(e.target.value)}}/><button className="send" onClick={handleSend}><img src={sendBtn} alt="Send" /></button>
+          </div>
+          <p> This is just clone of the chatGPT using react html css and OpenAPI</p>
+          </div>
       </div>
     </div>
   );
